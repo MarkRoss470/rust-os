a7d1e5426819a157781fb084b7b7fd1a04bc6d6b
Mark Ross <markross470@gmail.com>
2023-07-25 22:49:16 +0200

    Updated launch.sh to launch faster and offline

--- kernel/.vscode/launch.sh
+++ kernel/.vscode/launch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd $1/../kernel-builder
-cargo run -- --run --debug=$1/.vscode/log.txt 
+# Hardcoded path to nightly cargo rather than just 'cargo'
+# To prevent the default cargo binary from syncing nightly version every run.
+# This speeds up launches significantly and also allows running offline or on a bad connection.
+~/.rustup/toolchains/nightly-2023-06-20-x86_64-unknown-linux-gnu/bin/cargo run -- --run --debug=$1/.vscode/log.txt 
