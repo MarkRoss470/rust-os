32144fbc433646868b8327592221050d1bdeb15b
Mark Ross <markross470@gmail.com>
2023-08-27 18:11:54 +0100

    Refactored graphics to reduce deadlocks

--- kernel/src/graphics/mod.rs
+++ kernel/src/graphics/mod.rs
@@ -2,9 +2,10 @@
 
 mod font_const;
 
-use crate::{global_state::GlobalState};
+use crate::global_state::{GlobalState, TryLockedIfInitError};
 use bootloader_api::info::{FrameBuffer, FrameBufferInfo, PixelFormat};
 use core::fmt;
+use spin::Mutex;
 
 use self::font_const::FONT_BITMAPS;
 
@@ -140,7 +141,7 @@ pub struct Writer {
     /// The maximum height in rows the [`Writer`] can reach before scrolling the screen
     height: usize,
 
-    /// The current [`Colour`] of the text the [`Writer`] is rendering 
+    /// The current [`Colour`] of the text the [`Writer`] is rendering
     colour: Colour,
     /// The framebuffer the [`Writer`] is rendering into
     buffer: FrameBufferController,
@@ -203,6 +204,7 @@ impl fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
             self.write_char(c);
+            serial_print!("{c}");
         }
         Ok(())
     }
@@ -210,6 +212,17 @@ impl fmt::Write for Writer {
 
 /// The global [`Writer`] used by [`print!`][crate::print!] and [`println!`][crate::println!]
 pub static WRITER: GlobalState<Writer> = GlobalState::new();
+
+/// An error which can occur when trying to write to the screen
+#[derive(Debug, Clone, Copy)]
+enum WriteError {
+    /// The formatter passed to [`print!`] called [`print!`], leading to the inner call being unable to succeed.
+    Reentrancy,
+}
+
+/// An error which may have occurred while writing to the screen.
+/// Errors are stored here to indicate that writing failed.
+static WRITE_ERROR: Mutex<Option<WriteError>> = Mutex::new(None);
 
 /// Initialises the framebuffer.
 pub fn init_graphics(framebuffer: &'static mut FrameBuffer) {
@@ -239,10 +252,19 @@ pub fn _print(args: fmt::Arguments) {
     // Disable interrupts while locking mutex to prevent deadlock
     interrupts::without_interrupts(|| {
         // If the writer is not initialised, or is locked, return immediately without printing anything
-        if !WRITER.try_is_init().unwrap_or(false) {
-            return;
+        match WRITER.try_locked_if_init() {
+            Ok(mut lock) => {
+                lock.write_fmt(args).unwrap();
+            }
+            Err(TryLockedIfInitError::Locked) => {
+                if let Some(mut lock) = WRITE_ERROR.try_lock() {
+                    *lock = Some(WriteError::Reentrancy)
+                };
+            }
+            Err(TryLockedIfInitError::NotInitialised) => {
+                serial_print!("{args}");
+            },
         }
-        WRITER.lock().write_fmt(args).unwrap();
     });
 }
 
@@ -250,7 +272,6 @@ pub fn _print(args: fmt::Arguments) {
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ({
-        $crate::serial::_print(format_args!($($arg)*));
         $crate::graphics::_print(format_args!($($arg)*));
     });
 }
