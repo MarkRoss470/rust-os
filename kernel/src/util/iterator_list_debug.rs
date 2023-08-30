5fe7c923993a01b1855575c014b0793e68ceeabf
Mark Ross <markross470@gmail.com>
2023-08-30 11:58:23 +0100

    Added format specifier to IteratorListDebug Different constructor methods now produce different output by using a different formatter

--- kernel/src/util/iterator_list_debug.rs
+++ kernel/src/util/iterator_list_debug.rs
@@ -2,6 +2,16 @@
 
 use core::{fmt::Debug, cell::RefCell};
 
+/// What formatter the [`Debug`] impl of [`IteratorListDebug`] will pass to each element
+enum Format {
+    /// Pass on the formatter given
+    PassThrough,
+    /// Always use the default [`Debug`] formatter
+    DefaultDebug,
+    /// Always use the alternate (pretty-print) [`Debug`] formatter
+    AlternateDebug,
+}
+
 /// A utility struct for implementing [`Debug`] for an iterator such that it prints out all the values in a list format,
 /// without allocating.
 /// 
@@ -11,6 +21,16 @@ use core::{fmt::Debug, cell::RefCell};
 /// println!("{:?}", IteratorListDebug::new(iter)); // Prints "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]"
 /// 
 /// ```
+/// 
+/// If constructed with the [`new`] method, the formatter given to the [`Debug`]
+/// implementation will be passed on to each element, however if [`new_with_default_formatting`]
+/// or [`new_with_alternate_formatting`] are used then a new formatter is created with the given attributes.
+/// This can be used to make the output more compact, such as forcing the elements to print on one line each.
+/// 
+/// [`new`]: [IteratorListDebug::new]
+/// [`new_with_default_formatting`]: [IteratorListDebug::new_with_default_formatting]
+/// [`new_with_alternate_formatting`]: [IteratorListDebug::new_with_alternate_formatting]
+/// 
 pub struct IteratorListDebug<T, U>
 where
     T: Debug,
@@ -19,6 +39,8 @@ where
     /// The iterator, held in a [`RefCell`] so that the iterator can be iterated (requiring a mutable reference)
     /// inside the impl of [`Debug`], which gives a shared reference.
     iterator: RefCell<U>,
+    /// The [`Format`] which will be applied to the arguments
+    format: Format,
 }
 
 impl<T, U> IteratorListDebug<T, U>
@@ -26,8 +48,25 @@ where
     T: Debug,
     U: Iterator<Item = T>,
 {
+    /// Wraps the iterator in an [`IteratorListDebug`].
+    /// The implementation of [`Debug`] will pass through formatting arguments.
     pub fn new(iterator: U) -> Self {
-        Self { iterator: RefCell::new(iterator) }
+        Self { iterator: RefCell::new(iterator), format: Format::PassThrough }
+    }
+
+    /// Wraps the iterator in an [`IteratorListDebug`].
+    /// The implementation of [`Debug`] will always use the default debug format when debugging an item
+    /// (i.e. `format_args!("{item:?}")`).
+    pub fn new_with_default_formatting(iterator: U) -> Self {
+        Self { iterator: RefCell::new(iterator), format: Format::DefaultDebug }
+    }
+
+    /// Wraps the iterator in an [`IteratorListDebug`].
+    /// The implementation of [`Debug`] will always use the alternate debug format when debugging an item.
+    /// (i.e. `format_args!("{item:#?}")`).
+    /// For types whose [`Debug`] impls are macro derived, this usually means pretty printing the argument.
+    pub fn new_with_alternate_formatting(iterator: U) -> Self {
+        Self { iterator: RefCell::new(iterator), format: Format::AlternateDebug }
     }
 }
 
@@ -40,7 +79,15 @@ where
         let mut l = f.debug_list();
         let iterator = &mut *self.iterator.borrow_mut();
 
-        l.entries(iterator);
+        for entry in iterator {
+            match self.format {
+                Format::PassThrough => l.entry(&entry),
+                Format::DefaultDebug => l.entry(&format_args!("{entry:?}")),
+                Format::AlternateDebug => {
+                    l.entry(&format_args!("{entry:#?}"))
+                },
+            };
+        }
 
         l.finish()
     }
